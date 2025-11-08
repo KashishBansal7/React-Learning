fb1cad81a1ea1283c747071b9392815bb6251582
KashishBansal7 <kashishbansal002@gmail.com>
2025-11-08 13:09:09 +0530

    first code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/header";
+
+function App() {
+  return (
+    <>
+
+      <h1>hyy</h1>
+      <button>me hu button</button>
+      <Header />
+    </>
+  );
+}
+export default App;
---
+++ src/assets/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <>
+        <h1>React with Kashish</h1>
+        <h1>hy sumi </h1>
+        </>
+    )
+}
+export default Header;
